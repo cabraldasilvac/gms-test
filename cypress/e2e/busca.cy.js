3d67d21b237ed4378832baee6157c93411ea8c13
WANDERLEY CABRAL <39313251+cabraldasilvac@users.noreply.github.com>
2025-04-29 12:15:37 -0300

    feat: adiciona novas busca e testes

--- cypress/e2e/busca.cy.js
+++ cypress/e2e/busca.cy.js
@@ -10,22 +10,61 @@ describe('US-013-Funcionalidade: Busca de membros', () => {
         cy.get('#search-button').click()
         cy.get('#search-response').should('contain', 'Membro encontrado com sucesso!')
     });
+    
+    it('Deve buscar um membro por sobrenome', () => {
+        cy.get('#search').type('silva')
+        cy.get('#search-button').click()
+        cy.get('#search-response').should('contain', 'Membro encontrado com sucesso!')
+    });
+    it('Deve buscar um membro por email', () => {
+        cy.get('#search').type('email@teste.com')
+        cy.get('#search-button').click()
+        cy.get('#search-response').should('contain', 'Membro encontrado com sucesso!')
+    });
+    it('Deve buscar um membro por telefone', () => {
+        cy.get('#search').type('3199999999')
+        cy.get('#search-button').click()
+        cy.get('#search-response').should('contain', 'Membro encontrado com sucesso!')
+    }
+    );
+    it('Deve buscar um membro por nome inválido', () => {
+        cy.get('#search').type('wanderley58')
+        cy.get('#search-button').click()
+        cy.get('#search-response').should('contain', 'Membro não encontrado')
+    });
+    it('Deve buscar um membro por sobrenome inválido', () => {
+        cy.get('#search').type('silva123')
+        cy.get('#search-button').click()
+        cy.get('#search-response').should('contain', 'Membro não encontrado')
+    });
+    it('Deve buscar um membro por email inválido', () => {
+        cy.get('#search').type('email@teste')
+        cy.get('#search-button').click()
+        cy.get('#search-response').should('contain', 'Membro não encontrado')
+    });
+
+// Busca Filmes
+    it('Deve buscar um filme por nome', () => {
+        cy.get('#search.input').type('Matrix')
+        cy.get('#search-button').click()
+        cy.get('#results-section').should('contain', 'Matrix')
+    });
 
     it('Deve buscar um filme com sucesso.', () => {
         cy.get('#search.input').type('Matrix')
         cy.get('#search-button').click()
-        cy.get('#result-section').should('contain', 'Matrix')
+        cy.get('#results-section').should('contain', 'Matrix')
     });
 
-    it('Deve buscar filmes com sucesso de lista.', () => {
+    it.only('Deve buscar filmes com sucesso de lista.', () => {
         cy.fixture('filmes').then((filmes) => {
-                cy.get('#search-input').type(filmes[3].titulo)
+                cy.get('#search-input').type(filmes[2].titulo)
                 cy.get('#search-button').click()
-                cy.get('#result-section').should('contain', filmes[3].titulo)
+                cy.get('#results-section').should('contain', filmes[2].titulo)
             })
         });
 
-    it('Deve buscar filmes com sucesso d lista inteira.', () => {
+    it('Deve buscar filmes com sucesso na lista inteira.', () => {
         cy.fixture('filmes').each((filmes) => {
                 cy.get('#search-input').clear().type(filmes.titulo)
                 cy.get('#search-button').click({force: true})
--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -5,8 +5,12 @@
 describe('US-012-Funcionalidade: Cadastro de membros', () => {
 beforeEach(() => {
   cy.visit('/')
-});
+}
+);
 
+afterEach(() => {
+  cy.screenshot()
+});
 
   it('Deve fazer o cadastro de campos obrigatórios', () => {
    
@@ -15,10 +19,25 @@ beforeEach(() => {
 
     cy.preencherCadastro('wanderley', 'silva', email, '3199999999', 'Teste123@1289 ')
     cy.get('#signup-response').should('contain', 'Cadastro realizado com sucesso!')
-  })
+  }
+);
 
-  it.only('Deve validar mensagem de erro com o campo nome inválido', () => {
+  it('Deve validar mensagem de erro com o campo nome inválido', () => {
     cy.preencherCadastro('wanderley58', 'silva', 'email@teste.com', '3199999999', 'Teste123@1289 ')
     cy.get('#signup-response').should('contain', 'Nome deve conter apenas caracteres alfabéticos, acentuados e espaços')
-  });
-})
+  }
+);
+
+  it('Deve validar mensagem de erro com o campo sobrenome inválido', () => {
+    cy.preencherCadastro('wanderley', 'silva123', 'email@teste.com', '3199999999', 'Teste123@1289 ')
+    cy.get('#signup-response').should('contain', 'Sobrenome deve conter apenas caracteres alfabéticos, acentuados e espaços')
+  }
+);
+
+  it('Deve validar mensagem de erro com o campo telefone inválido', () => {
+    cy.preencherCadastro('wanderley', 'silva', 'email@teste.com', 'tel319999999', 'Teste123@1289 ')
+    cy.get('#signup-response').should('contain', 'Telefone deve conter apenas números')
+  }
+  );
+
+});
